7226fdeaf33140a300e75074311e04ba5ec0fd51
gurjot-singh02 <98311531+gurjot-singh02@users.noreply.github.com>
2022-01-28 11:27:08 +0530

    Add files via upload

---
+++ Untitled.js
@@ -0,0 +1,65 @@
+//array functions
+// let array = [
+//     {
+//         name: 'xyz',
+//         age: 10
+//     },
+//     {
+//         name: 'ert',
+//         age: 4
+//     },
+//     {
+//         name: 'mvm',
+//         age: 45
+//     },
+//     {
+//         name: 'rere',
+//         age: 56
+//     },
+// ]
+
+
+// let arraytwo = [
+//     {
+//         name: 'fdcd',
+//         age: 32
+//     },
+//     {
+//         name: 'ter',
+//         age: 1
+//     },
+//     {
+//         name: 'fdfg',
+//         age: 3
+//     },
+//     {
+//         name: 'gfdgd',
+//         age: 15
+//     },
+// ]
+// console.log(array3 = [...array, ...arraytwo])
+
+n1 = 'Hello'
+n2 = 'World'
+n3 = 'Hello World'
+
+for (let i = 1; i <= 30; i++) {
+    if (i % 15 == 0) {
+        console.log(n3);
+    }
+    else if (i % 5 == 0) {
+        console.log(n2);
+    }
+    else if (i % 3 == 0) {
+        console.log(n1)
+    }
+    else {
+        console.log(i)
+    }
+}
+
+
+
+
+
+
